7ab273c793d22e7c95cda2f29522ac27d80ac5e9
Zain <zazimran@gmail.com>
2023-03-16 12:14:43 +0500

    fixed and added some stuff

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@ if($_POST["name"]) {
     mail("zazimran@gmail.com", "From ". $_POST["name"]. " of " . $_POST["company"] ,
     $_POST["message"]. " From: " . $_POST["email"], "CC: universalunisoft@gmail.com" . "CC: info@zazsoft.com");
 
-    header("contact.html");
+    header("Location: contact.html");
     }
 else {
-    header("contact.html");
+    header("Location: contact.html");
 }
 
 ?>
