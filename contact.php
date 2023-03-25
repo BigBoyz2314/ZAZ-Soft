a334564ddcd1ae12cbfe5c6674cce51a82a9d186
Zain <zazimran@gmail.com>
2023-03-25 15:33:23 +0500

    fixed email

--- contact.php
+++ contact.php
@@ -2,7 +2,13 @@
 
 if($_POST["name"]) {
     mail("zazimran@gmail.com", "From ". $_POST["name"]. " of " . $_POST["company"] ,
-    $_POST["message"]. " From: " . $_POST["email"], "CC: universalunisoft@gmail.com" . "CC: info@zazsoft.com");
+    $_POST["message"]. " From: " . $_POST["email"]);
+
+    mail("universalunisoft@gmail.com", "From ". $_POST["name"]. " of " . $_POST["company"] ,
+    $_POST["message"]. " From: " . $_POST["email"]);
+
+    mail("info@zazsoft.com", "From ". $_POST["name"]. " of " . $_POST["company"] ,
+    $_POST["message"]. " From: " . $_POST["email"]);
 
     header("Location: contact.html");
     }
